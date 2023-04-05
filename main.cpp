bde4e338fc6e8e682a40da92ea80d61d38f2a0bc
Sorin <93950240+Sorin373@users.noreply.github.com>
2023-04-05 20:43:14 +0300

    Added dynamic allocation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <string.h>
+#include <cstring>
 
 #include "functions.hpp"
 
@@ -10,18 +11,33 @@ using namespace std;
 const int MAXN = 100;
 
 int n, v[MAXN];
-bool adj[MAXN][MAXN];
-char nume[100][101];
+bool **adj;
+char **nume;
 
 void citireDate()
 {
     ifstream fin("data.in");
     fin >> n;
-    for (unsigned int i = 0; i < n; i++)
+
+    nume = new char *[n];
+    for (int i = 0; i < n; i++)
+        nume[i] = new char[101];
+
+    for (int i = 0; i < n; i++)
         fin >> nume[i];
+
+    adj = new bool *[n + 1];
+    for (int i = 1; i <= n; i++)
+        adj[i] = new bool[n + 1];
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            adj[i][j] = false;
+
     int x, y;
     while (fin >> x >> y)
         adj[x][y] = adj[y][x] = true;
+
     fin.close();
 }
 
@@ -43,7 +59,8 @@ void verifcarePrieten()
 {
     clear_screen();
 
-    char numeCautat_1[101], numeCautat_2[101];
+    char *numeCautat_1 = new char[101];
+    char *numeCautat_2 = new char[101];
     bool exista = false;
 
     cout << "\n\n";
@@ -59,21 +76,29 @@ void verifcarePrieten()
          << "Nume_1: ";
     cin >> numeCautat_1;
     if (strcmp(numeCautat_1, "0") == 0)
+    {
+        delete[] numeCautat_1;
+        delete[] numeCautat_2;
         return;
+    }
     cout << setw(5) << " "
          << "Nume_2: ";
     cin >> numeCautat_2;
     if (strcmp(numeCautat_2, "0") == 0)
+    {
+        delete[] numeCautat_1;
+        delete[] numeCautat_2;
         return;
+    }
     else
     {
         cout << "\n";
-        for (unsigned int i = 1; i <= n && !exista; i++)
-            for (unsigned int j = 1; j <= n && !exista; j++)
-                if (adj[i][j] == 1)
+        for (int i = 1; i <= n && !exista; i++)
+            for (int j = 1; j <= n && !exista; j++)
+                if (adj[i][j])
                 {
                     if (strcasecmp(nume[i - 1], numeCautat_1) == 0 && strcasecmp(nume[j - 1], numeCautat_2) == 0)
-                    {   
+                    {
                         cout << setw(5) << " " << numeCautat_1 << " este prieten cu " << numeCautat_2;
                         exista = true;
                     }
@@ -83,7 +108,12 @@ void verifcarePrieten()
                         exista = true;
                     }
                 }
-        if (!exista) cout << setw(5) << " " << "Nu exista aceasta prietenie!";
+        if (!exista)
+            cout << setw(5) << " "
+                 << "Nu exista aceasta prietenie!";
+
+        delete[] numeCautat_1;
+        delete[] numeCautat_2;
 
         getch();
         verifcarePrieten();
@@ -93,21 +123,20 @@ void verifcarePrieten()
 void gradNod(int i)
 {
     int contor = 0;
-    if (i > n)
+    if (i >= n)
         return;
     else
-    { 
-        if (i != 0)
-            cout << setw(5) << " " << nume[i - 1] << ": ";
-        for (unsigned int j = 0; j < n; j++)
-            if (adj[i][j] == 1)
+    {
+        cout << setw(5) << " " << nume[i - 1] << ": ";
+        for (int j = 1; j <= n; j++)
+            if (adj[i][j])
             {
-                if (j == n - 1) cout << nume[j - 1];
-                else cout << nume[j - 1] << ", ";
+                if (contor > 0)
+                    cout << ", ";
+                cout << nume[j - 1];
                 contor++;
             }
-        if (i != 0)
-            cout << "(" << contor << " prieteni)\n";
+        cout << " (" << contor << " prieteni)\n";
 
         gradNod(i + 1);
     }
@@ -116,12 +145,12 @@ void gradNod(int i)
 int gradMaxRec(int i, int contorV)
 {
     int contor = 0;
-    if (i > n)
+    if (i >= n)
         return contorV;
     else
     {
-        for (unsigned int j = 0; j < n; j++)
-            if (adj[i][j] == 1)
+        for (int j = 1; j <= n; j++)
+            if (adj[i][j])
                 contor++;
         v[contorV] = contor;
         gradMaxRec(i + 1, contorV + 1);
@@ -130,15 +159,18 @@ int gradMaxRec(int i, int contorV)
 
 void gradMax()
 {
-    int maxim = -(n * (n - 1)) / 2, contorV = gradMaxRec(0, 0);
+    int maxim = -(n * (n - 1)) / 2, contorV = gradMaxRec(1, 0);
     for (unsigned int i = 0; i < contorV; i++)
         if (v[i] > maxim)
             maxim = v[i];
-    cout << "\n" << setw(5) << " " << "Numarul maxim de prietenii este: " << maxim << "\n"
-         << setw(5) << " " << "Persoanele cu numarul acesta de prieteni sunt: ";
+    cout << "\n"
+         << setw(5) << " "
+         << "Numarul maxim de prietenii este: " << maxim << "\n"
+         << setw(5) << " "
+         << "Persoanele cu numarul acesta de prieteni sunt: ";
     for (unsigned int i = 0; i < contorV; i++)
         if (v[i] == maxim)
-            cout << nume[i - 1] << " ";
+            cout << nume[i] << " ";
 }
 
 void grafComplet()
@@ -148,10 +180,41 @@ void grafComplet()
         for (unsigned int j = 1; j < n; j++)
             if (adj[i][j] == 1 && i < j)
                 contor++;
-    cout << "\n" << setw(5) << " " << (n * (n - 1)) / 2 
-         << " muchii - " << contor << " muchii ramase = " 
-         << (n * (n - 1)) / 2 - contor 
+    cout << "\n"
+         << setw(5) << " " << (n * (n - 1)) / 2
+         << " muchii - " << contor << " muchii ramase = "
+         << (n * (n - 1)) / 2 - contor
          << " muchii pana la un graf complet";
+}
+
+void eliberareMemorie()
+{
+    if (nume != nullptr)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (nume[i] != nullptr)
+            {
+                delete[] nume[i];
+                nume[i] = nullptr;
+            }
+        }
+        delete[] nume;
+        nume = nullptr;
+    }
+
+    if (adj != nullptr)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (adj[i] != nullptr)
+            {
+                delete[] adj[i];
+                adj[i] = nullptr;
+            }
+        }
+        adj = nullptr;
+    }
 }
 
 int main()
@@ -192,7 +255,7 @@ int main()
             break;
         case 2:
             cout << "\n";
-            gradNod(0);
+            gradNod(1);
             getch();
             break;
         case 3:
@@ -205,5 +268,8 @@ int main()
             break;
         }
     } while (meniu != 0);
+
+    eliberareMemorie();
+
     return 0;
 }
